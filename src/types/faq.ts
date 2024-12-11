6fed42a154db5304955d9dcc7f31abf066f4c859
tieee7 <testingtestt36@gmail.com>
2024-12-11 23:43:00 +0100

    Delete src directory

--- src/types/faq.ts
+++
@@ -1,7 +0,0 @@
-export interface FAQ {
-    id: string;
-    question: string;
-    answer: string;
-    domain_id: string;
-    created_at: string;
-  }
--- src/types/trainingData.ts
+++
@@ -1,6 +0,0 @@
-export interface TrainingData {
-    id: number | string;
-    content: string;
-    domain_id: string;
-    created_at: string;
-}
